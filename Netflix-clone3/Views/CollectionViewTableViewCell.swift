7d43bba4ba1d46fa936fe62d96c9e5040167b1ba
AndrewBorisov81 <ibobojohn81@gmail.com>
2024-12-12 19:45:17 +0200

    Added CollectionViewTableViewCell

---
+++ Netflix-clone3/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-clone3
+//
+//  Created by Andrew Borisov on 12.12.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
